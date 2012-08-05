24dd1f471f15109b29926634e6e9921f3b045c79
Bryan Taylor <btaylor@alpharender.net>
2012-08-04 22:02:50 -0400

    Set presence must be true for site, location, Itype for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :site
   belongs_to :location
   belongs_to :itype
+  validates :site_id, :location_id, :itype_id, :presence => true
   attr_accessible :serial, :stockable, :site_id, :location_id, :itype_id
 end

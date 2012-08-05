4dc4b89cb3921c2edf43118f8f7a4891acf4c110
Bryan Taylor <btaylor@alpharender.net>
2012-08-05 01:39:02 -0400

    fixes #1 Modified initial migration to create proper columns, removed monkey fix migration

--- db/migrate/20120804215822_rename_i_ds.rb
+++
@@ -1,7 +0,0 @@
-class RenameIDs < ActiveRecord::Migration
-  def change
-	rename_column :items, :location_id_id, :location_id
-	rename_column :items, :site_id_id, :site_id
-	rename_column :items, :itype_id_id, :itype_id
-  end
-end
